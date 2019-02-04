55668bff68a916f566816c9723d39d5c920de886
Jordan Duran <jordanduranm@gmail.com>
2019-02-04 20:30:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function sayHiToGrandma(string) {
   return 'I can\'t hear you!';
   }
   else if (string === "I love you, Grandma.")
+  return "I Love you too"
 }
 
 function sayHiToGrandma(string) {
